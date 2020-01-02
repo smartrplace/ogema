c7e71699136c8b018bc3aae60df7dc638be07432
Jan Lapp <jan.lapp@smartrplace.de>
2020-01-02 16:26:38 +0100

    fixed main POM for 2.2.2-SNAPSHOT build, added homematic channel handler for HmIP-SK9

---
+++ src/drivers/homematic-xmlrpc-hl/src/main/java/org/ogema/drivers/homematic/xmlrpc/hl/channels/IpThermostatBChannelHandlerFactory.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright 2011-2018 Fraunhofer-Gesellschaft zur Förderung der angewandten Wissenschaften e.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ogema.drivers.homematic.xmlrpc.hl.channels;
+
+import org.osgi.service.component.annotations.Component;
+import org.ogema.drivers.homematic.xmlrpc.hl.api.DeviceHandler;
+import org.ogema.drivers.homematic.xmlrpc.hl.api.DeviceHandlerFactory;
+import org.ogema.drivers.homematic.xmlrpc.hl.api.HomeMaticConnection;
+import org.osgi.framework.Constants;
+
+/**
+ *
+ * @author jlapp
+ */
+@Component(service = {DeviceHandlerFactory.class}, property = {Constants.SERVICE_RANKING + ":Integer=1"})
+public class IpThermostatBChannelHandlerFactory implements DeviceHandlerFactory {
+
+    @Override
+    public DeviceHandler createHandler(HomeMaticConnection connection) {
+        return new IpThermostatBChannel(connection);
+    }    
+    
+}
