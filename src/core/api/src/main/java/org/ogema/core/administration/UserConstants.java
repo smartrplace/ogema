ebfd4e97876d4aaefa9982bde3eb470a2c828e71
David Nestle <david.nestle@iee.fraunhofer.de>
2020-08-26 16:07:45 +0200

    added email-invite and locale to UserConstants

--- src/core/api/src/main/java/org/ogema/core/administration/UserConstants.java
+++ src/core/api/src/main/java/org/ogema/core/administration/UserConstants.java
@@ -10,6 +10,10 @@ public final class UserConstants {
     
     public static final String FORMATTED_NAME = "user.formattedName";
     
+    public static final String SEND_INVITATION_BY_EMAIL = "user.inviteByMail";
+    
+    public static final String PREFERRED_LOCALE = "user.locale";
+    
     private UserConstants() {}
     
 }
