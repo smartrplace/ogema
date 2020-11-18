dedae0731a5d49d94fb3930c9cbd37f7024dba72
Jan Lapp <jan.lapp@smartrplace.de>
2020-11-18 13:15:27 +0100

    core api: added new energy units kWh and kvarh and setUnit method on EnergyResource

--- src/core/ref-impl/resource-manager/src/main/java/org/ogema/resourcemanager/impl/model/units/DefaultEnergyResource.java
+++ src/core/ref-impl/resource-manager/src/main/java/org/ogema/resourcemanager/impl/model/units/DefaultEnergyResource.java
@@ -16,27 +16,74 @@
 package org.ogema.resourcemanager.impl.model.units;
 
 import org.ogema.core.model.Resource;
+import org.ogema.core.model.simple.StringResource;
 import org.ogema.core.model.units.EnergyResource;
 import org.ogema.core.model.units.PhysicalUnit;
 import org.ogema.resourcemanager.impl.ApplicationResourceManager;
 
 import org.ogema.resourcemanager.virtual.VirtualTreeElement;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author Timo Fischer, Fraunhofer IWES
  */
 public class DefaultEnergyResource extends UnitFloatResource implements EnergyResource {
+    
+    public static final String UNIT_DECORATOR = "unit";
+    private StringResource unitDec;
+    PhysicalUnit unit = PhysicalUnit.JOULES;
 
 	public DefaultEnergyResource(VirtualTreeElement el, Class<? extends Resource> unitResourceType, String path,
 			ApplicationResourceManager resMan) {
 		super(el, unitResourceType, path, resMan);
 	}
 
+    private void initUnit() {
+        unitDec = getSubResource(UNIT_DECORATOR, StringResource.class);
+        if (isActive()) {
+            if (unitDec.getValue() != null && !unitDec.getValue().isEmpty()) {
+                unit = PhysicalUnit.fromString(unitDec.getValue());
+            } else {
+                unitDec.create();
+                unitDec.setValue(PhysicalUnit.JOULES.toString());
+                unitDec.activate(false);
+            }
+        }
+    }
+
 	@Override
 	public final PhysicalUnit getUnit() {
+        try {
+            if (unitDec == null) {
+                initUnit();
+            }
+            if (unitDec.getValue() != null && !unitDec.getValue().isEmpty()) {
+                return PhysicalUnit.fromString(unitDec.getValue());
+            }
+        } catch (RuntimeException e) {
+            LoggerFactory.getLogger(DefaultEnergyResource.class)
+                    .error("invalid unit value on {}", getPath(), e);
+        }
 		return PhysicalUnit.JOULES;
 	}
+    
+    @Override
+    public final void setUnit(PhysicalUnit u) {
+        try {
+            if (unitDec == null) {
+                initUnit();
+            }
+            unitDec.create();
+            unitDec.setValue(u.toString());
+            if (isActive()) {
+                unitDec.activate(false);
+            }
+        } catch (RuntimeException e) {
+            LoggerFactory.getLogger(DefaultEnergyResource.class)
+                    .error("could not set unit value on {}", getPath(), e);
+        }
+    }
 
 	@Override
 	public float getKWhs() {
