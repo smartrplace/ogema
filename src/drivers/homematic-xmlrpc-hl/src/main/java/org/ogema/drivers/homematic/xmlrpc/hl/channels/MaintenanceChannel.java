3647e5b8b436b1a1af374a27647ecab73ec8558f
Jan Lapp <jan.lapp@smartrplace.de>
2021-03-31 14:01:17 +0200

    homematic-xmlrpc-hl: fixed mis-named decorator in maintenance channel

--- src/drivers/homematic-xmlrpc-hl/src/main/java/org/ogema/drivers/homematic/xmlrpc/hl/channels/MaintenanceChannel.java
+++ src/drivers/homematic-xmlrpc-hl/src/main/java/org/ogema/drivers/homematic/xmlrpc/hl/channels/MaintenanceChannel.java
@@ -193,7 +193,7 @@ public class MaintenanceChannel extends AbstractDeviceHandler {
                 break;
             }
             case DUTY_CYCLE : {
-                GenericBinarySensor sens = sd.getSubResource("dutyCycleLevel", GenericBinarySensor.class);
+                GenericBinarySensor sens = sd.getSubResource("dutyCycle", GenericBinarySensor.class);
                 if (!sens.isActive()) {
                     sens.reading().create();
                     sens.reading().activate(false);
