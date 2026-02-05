0fa84c73948d6799ffe636595a0928a3ab4eae86
Jan Lapp <jan.lapp@smartrplace.de>
2026-02-05 09:45:55 +0100

    models: added CentralHeating model

---
+++ src/core/models/src/main/java/org/ogema/model/devices/buildingtechnology/CentralHeating.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright 2011-2018 Fraunhofer-Gesellschaft zur Förderung der angewandten Wissenschaften e.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ogema.model.devices.buildingtechnology;
+
+import org.ogema.core.model.ResourceList;
+import org.ogema.model.actors.OnOffSwitch;
+import org.ogema.model.actors.MultiSwitch;
+import org.ogema.model.connections.FluidConnection;
+import org.ogema.model.connections.ThermalMixingConnection;
+import org.ogema.model.devices.generators.HeatGenerator;
+import org.ogema.model.devices.storage.ThermalStorage;
+import org.ogema.model.prototypes.PhysicalElement;
+import org.ogema.model.sensors.EnergyAccumulatedSensor;
+
+/**
+ * Central heating model.
+ */
+public interface CentralHeating extends PhysicalElement {
+	/**
+	 * On/off switch for the whole device.
+	 */
+	OnOffSwitch onOffSwitch();
+
+	/**
+	 * Combined power setting of heat and electricity as a ratio of the rated
+	 * values. The settings are relative values referring to the rated powers
+	 * of {@link #electricityGenerator() } and {@link #heatGenerator() }
+	 */
+	MultiSwitch setting();
+
+	/** Heat generating unit, e.g. burner */
+	HeatGenerator heatGenerator();
+
+	/**
+	 * Storage for domestic (drinking) water
+	 */
+	ThermalStorage domesticStorage();
+
+	/**
+	 * Storage for heating.
+	 */
+	ThermalStorage heatingStorage();
+
+	/**
+	 * Connection to fuel input
+	 */
+	FluidConnection fuelConnection();
+	
+	/**
+	 * Stores total primary energy used, total heat energy
+	 * should be stored on the {@link #heatGenerator() }
+	 * 
+	 * @return total primary energy used.
+	 */
+	EnergyAccumulatedSensor primaryEnergy();
+	
+	/**
+	 * Additional mixing circuits for heating.
+	 * 
+	 * @return connected mixing circuits.
+	 */
+	ResourceList<ThermalMixingConnection> mixingCircuits();
+
+}
